99ef998abf966514e4e3f495347960c21cb1b732
root <root@DEBIAN.myguest.virtualbox.org>
2026-08-28 11:00:31 +0200

    Squelette : structure Terraform multi-cibles (ecs + k8s)

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+module "k8s" {
+  source = "./modules/k8s"
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+}
